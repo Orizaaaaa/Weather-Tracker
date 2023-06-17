dac2e3b5311d0a0f42508a7cea4b3916e605eba1
oryzaaa <oryzasativacikal@gmail.com>
2023-06-17 17:40:12 +0700

    menambahkan UI

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 let InputKyword = document.querySelector('.input-kyword')
 let buttonSearch = document.querySelector('.button-search')
+buttonSearch.addEventListener('click', function () {
+    fetch(' https://api.openweathermap.org/data/2.5/weather?q=' + InputKyword.value + '&appid=705a6deb5cbd0a027f801d77faf8af73&units=matric ')
+        .then(response => response)
+        .then((response) =>
+            function () {
+                let result = document.querySelector('.result')
+                result.innerHTML = ` `
+            })
+})
